3bb1a37a504b5b0aea511e5b9af533c0dd0d57b9
AKamilla <sunkamri@gmail.com>
2024-06-04 14:04:12 +0200

    L

---
+++ 2021/L_slow.py
@@ -0,0 +1,76 @@
+
+def next(x:int, y:int, c:chr, matrix: list[list[chr]]):
+    for i in range(x, len(matrix[0])):
+        if matrix[y][i] == c:
+            return i - x
+    return 1e1000
+
+
+def area(matrix:list[list[chr]], x:int, y:int, w:int, h:int) -> int:
+    area = 1e1000
+    for c_h in range(1, h - y + 1): #O(w * h * h)
+        max_c = 0
+        for c in "WALDO": #O(5 * w * h)
+            min_h = 1e1000
+            for k in range(0, c_h): #O(w * h)
+                n = next(x, y + k, c, matrix) # O(w)
+                if n < min_h:
+                    min_h = n
+            if min_h > max_c:
+                max_c = min_h
+
+        c_w = max_c + 1
+
+        if c_h * c_w < area:
+            area = c_h * c_w
+    return area
+
+TESTS = [
+    """5 5
+ABCDE
+FGHIJ
+KLMNO
+PQRST
+VWXYZ""",
+    """5 10
+ABCDEABCDE
+FGHIJFGHIJ
+KLMNOKLMNO
+PQRSTPQRST
+VWXYZVWXYZ""",
+    """5 10
+WAALDLODOW
+AWWLAOODOW
+LOLADOWALO
+ADALLLWWOL
+WWOOAAAALO""",
+    """2 3
+WAL
+TER"""
+]
+
+
+def read_input(s:str):
+    lines = s.strip().split('\n')
+    list1 = list(map(int, lines[0].split(' ')))
+    h = list1[0]
+    w = list1[1]
+    matrix = [[] * w] * h
+    for i in range(1, h + 1):
+        matrix[i - 1] = list(lines[i])
+    return h, w, matrix
+
+if __name__ == '__main__':
+
+    for i in range(4):
+        h, w, matrix = read_input(TESTS[i])
+        min_area = 1e1000
+        for x in range(w):
+            for y in range(h):
+                a = area(matrix, x, y, w, h)
+                if a < min_area:
+                    min_area = a
+        if min_area >= 1e1000:
+            print("impossible")
+        else:
+            print(min_area)
