e78608de7a59c8b7d61205bdb0d1ce38452dd430
Ignacio G <garcia.nunez@tum.de>
2024-06-05 11:31:59 +0200

    Add caches to 2021/L

--- 2021/L_slow.py
+++ 2021/L_slow.py
@@ -1,19 +1,22 @@
+from functools import cache
+from time import time
 
-def next(x:int, y:int, c:chr, matrix: list[list[chr]]):
+
+def next(x:int, y:int, c:chr):
     for i in range(x, len(matrix[0])):
         if matrix[y][i] == c:
             return i - x
     return 1e1000
 
 
-def area(matrix:list[list[chr]], x:int, y:int, w:int, h:int) -> int:
+def area(x:int, y:int, h:int) -> int:
     area = 1e1000
     for c_h in range(1, h - y + 1): #O(w * h * h)
         max_c = 0
         for c in "WALDO": #O(5 * w * h)
             min_h = 1e1000
             for k in range(0, c_h): #O(w * h)
-                n = next(x, y + k, c, matrix) # O(w)
+                n = next(x, y + k, c) # O(w)
                 if n < min_h:
                     min_h = n
             if min_h > max_c:
@@ -60,16 +63,20 @@ def read_input(s:str):
         matrix[i - 1] = list(lines[i])
     return h, w, matrix
 
+
 if __name__ == '__main__':
 
-    for i in range(4):
+    for i in range(len(TESTS)):
         h, w, matrix = read_input(TESTS[i])
         min_area = 1e1000
+        start = time()
         for x in range(w):
             for y in range(h):
-                a = area(matrix, x, y, w, h)
+                a = area(x, y, h)
                 if a < min_area:
                     min_area = a
+        end = time()
+        print(f"Test {i + 1} took {end - start} s")
         if min_area >= 1e1000:
             print("impossible")
         else:
